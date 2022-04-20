b0da06d20192383fe538e0c56d8d60f8da5cd527
Bryan Koh <s10223617@connect.np.edu.sg>
2022-04-21 01:21:11 +0800

    question 3

--- Question3.java
+++ Question3.java
@@ -11,6 +11,9 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    //System.out.print("Please enter an integer: ");
+    int integer = in.nextInt();
+    int result = integer * integer;
+    System.out.println(result);
   }
 }
